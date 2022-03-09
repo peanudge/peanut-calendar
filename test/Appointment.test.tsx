ca818d2a433ef127f05152ee00ebf5fdd12fcba1
peanut-lover <jihoson94@gmail.com>
2022-03-09 15:55:23 +0900

    Refactoring Appointment.test.tsx

--- test/Appointment.test.tsx
+++ test/Appointment.test.tsx
@@ -3,18 +3,26 @@ import ReactDOM from "react-dom";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+    let container: ReactDOM.Container | null = null;
+    let customer;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+    })
+
+    const render = (
+      component: React.ReactElement
+    ) => ReactDOM.render(component, container);
 
     it("renders the customer first name", () => {
         const customer = { firstName: "Ashley" }
-        const container = document.createElement('div');
-        ReactDOM.render(<Appointment customer={customer} />, container);
-        expect(container.textContent).toMatch("Ashley");
+        render(<Appointment customer={customer} />);
+        expect(container!.textContent).toMatch("Ashley");
     });
-    
+
     it("renders another customer first name", () => {
         const customer = { firstName: "Jordan" };
-        const container = document.createElement("div");
-        ReactDOM.render(<Appointment customer={customer} />, container);
-        expect(container.textContent).toMatch("Jordan");
+        render(<Appointment customer={customer} />);
+        expect(container!.textContent).toMatch("Jordan");
     });
  });
